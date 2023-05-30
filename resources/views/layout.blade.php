5a8beebb740873a552b21f0eed5e3cf810326ea8
waelprogrammer@yahoo.com <waelporgrammer@yahoo.com>
2023-05-30 20:58:19 +0300

    update main layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -27,19 +27,31 @@
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
-                    @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+            <div class="flex-1 px-3 py-3 text-xl font-bold text-teal-800 brand">
+                <a href="/">Chat App</a>
+            </div>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+            <div class="px-6 py-3 sm:block">
+            @auth
+                <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">
+                    Hello {{auth()->user()->name}}
+                </a>
+
+                <a href="{{ route('logout') }}" class="text-sm text-gray-700 dark:text-gray-500 underline" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    Logout
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none">
+                    {{csrf_field()}}
+                </form>
+            @else
+                <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                @endif
+            @endauth
+            </div>
         </div>
 
         @yield('content')
