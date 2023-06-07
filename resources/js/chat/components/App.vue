105f7c605c290eae412315067e50426f18613da4
waelprogrammer@yahoo.com <waelporgrammer@yahoo.com>
2023-06-07 11:12:17 +0300

    display online users

--- resources/js/chat/components/App.vue
+++ resources/js/chat/components/App.vue
@@ -28,7 +28,6 @@ export default {
 
         async function getOnlineUsers() {
             const result = await window.axios.get("/online-users");
-            console.log(result);
 
             if(result.data.users) {
                 onlineUsers.value = result.data.users;
