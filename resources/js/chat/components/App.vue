f66d503b170bd554193af98bcdd03eb065e59f00
waelprogrammer@yahoo.com <waelporgrammer@yahoo.com>
2023-06-07 11:43:07 +0300

    handling user click

--- resources/js/chat/components/App.vue
+++ resources/js/chat/components/App.vue
@@ -4,7 +4,7 @@
         <ul class="mt-3 mx-3 list-none inline-block">
             <li v-for="user in users" v-bind:key="user.id" class="mb-4">
                 <i class="fas fa-comment text-teal-700 px-2"></i>
-                <a href="#" class="text-teal-700 hover:text-teal-900 underline">{{user.name}}</a>
+                <a href="#" class="text-teal-700 hover:text-teal-900 underline" v-on:click.prevent="showChatPanel(user)">{{user.name}}</a>
             </li>
         </ul>
     </div>
@@ -15,7 +15,7 @@
 
 <script>
 
-import {provide, ref} from "vue";
+import {provide, ref, reactive} from "vue";
 
 export default {
     name: "App",
@@ -26,6 +26,10 @@ export default {
 
         const onlineUsers = ref([]);
 
+        // storing array of chat panels of clicked users
+        // chat panel is an object of this form {selectedUser: null, emittedMessage: null}
+        let chatPanels = reactive({panels: []});
+
         async function getOnlineUsers() {
             const result = await window.axios.get("/online-users");
 
@@ -34,10 +38,38 @@ export default {
             }
         }
 
+        function showChatPanel(user, emittedMessage = null) {
+            const isPanelOpened = chatPanels.panels.find(panel => panel.selectedUser.id === user.id);
+
+            if(!isPanelOpened) {
+                const userPanel = {
+                    selectedUser: user,
+                    emittedMessage
+                };
+
+                chatPanels.panels.push(userPanel);
+                return true;
+            }
+
+            // if the panel already opened
+            const index = chatPanels.panels.findIndex(panel => panel.selectedUser.id === user.id);
+
+            chatPanels.panels[index] = {...chatPanels.panels[index], emittedMessage};
+            return false;
+        }
+
+        function hideChatPanel(user) {
+            const filtered = [...chatPanels.panels].filter(panel => panel.selectedUser.id !== user.id);
+
+            chatPanels.panels = [...filtered];
+        }
+
         getOnlineUsers();
 
         return {
-            users: onlineUsers
+            users: onlineUsers,
+            showChatPanel,
+            hideChatPanel
         }
     }
 }
